8dacf9334e11d3f6b6db7eff2638e16821a6f772
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-02-13 09:06:35 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,3 +5,14 @@ const path    = require('path'),
       resolve = path.resolve;
 
 const getConfig = require('hjs-webpack');
+
+const root    = resolve(__dirname);
+const src     = join(root, 'src');
+const modules = join(root, 'node_modules');
+const dest    = join(root, 'dist');
+
+var config = getConfig({
+  in: join(__dirname, 'src/app.js'),
+  out: join(__dirname, 'dist'),
+  clearBeforeBuild: true
+})

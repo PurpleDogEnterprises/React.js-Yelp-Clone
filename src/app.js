b833cd5ca07f2cc4cffd3d3b62e3b8f507458dda
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-02-14 15:25:59 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 
 const App = React.createClass({
   render: function() {
-    return (<div>Text text text</div>);
+    return (<div>Text text text</div>)
   }
 });
+
+const mountNode = document.querySelector('#root');
+ReactDOM.render(<App />, mountNode);
